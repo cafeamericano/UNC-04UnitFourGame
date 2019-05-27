a6bb58b67a7d02304ff442481d05d1dd35293c54
Matthew <mfarmer5102@gmail.com>
2019-05-27 15:57:08 -0400

    Successfully implemented most of the battle logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,42 @@
 //WAIT FOR DOCUMENT READY
 $(document).ready(function () {
 
+    //GLOBAL VARIABLES////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     //OBJECTS////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     //********************************** HUD *********************************************
     var HUD = {
         drawPlayerHP: function () {
-            $("#game-window").append("<p id='playerHPLabel'>HP:</p>")
-            $("#game-window").append("<p id='playerHP'></p>")
+            $("#game-window").append("<span id='playerHPLabel'>HP:</span>")
+            $("#game-window").append("<span id='playerHP'></span><br/>")
             player.updateHP()
         },
 
         drawPlayerAttackPower: function () {
-            $("#game-window").append("<p id='playerAttackPowerLabel'>Attack Power:</p>")
-            $("#game-window").append("<p id='playerAttackPower'></p>")
+            $("#game-window").append("<span id='playerAttackPowerLabel'>Attack Power:</span>")
+            $("#game-window").append("<span id='playerAttackPower'></span><br/>")
             player.updateAttackPower()
+        },
+
+        drawAttackCounter: function () {
+            $("#game-window").append("<span id='attackCounterLabel'>Number of Attacks:</span>")
+            $("#game-window").append("<span id='attackCounter'></span><br/>")
+            player.updateAttackCounter()
         }
     };
 
     //********************************** PLAYER *********************************************
     var player = {
         HP: 100,
+        attackCounter: 0,
         baseAttackPower: 5,
         attackPower: 5,
 
         initialDraw: function () {
             $("#game-window").append("<div id='player'>Character</div>")
             $("#player").css({ border: "2px solid white", width: "150px", height: "150px" })
-            $("#player").css({ position: "absolute", left: "50px", bottom: "250px" })
+            $("#player").css({ position: "absolute", left: "50px", bottom: "235px" })
             this.updateHP();
         },
 
@@ -38,12 +47,63 @@ $(document).ready(function () {
 
         updateAttackPower: function () {
             $("#playerAttackPower").text(`${this.attackPower}`)
+        },
+
+        updateAttackCounter: function () {
+            $("#attackCounter").text(`${this.attackCounter}`)
         }
+    };
+
+    //********************************** DINO *********************************************
+    var dino = {
+        HP: 200,
+        counterAttackPower: 10,
+        isDefending: false,
+
+        defendingActivate: function () {
+            $('#dino').animate({ 'left': '450px' });
+            this.isDefending = true;
+        },
+
+        defendingDeactivate: function () {
+            $('#dino').animate({ 'left': '600px' });
+            this.isDefending = false;
+        },
+
+        initialDraw: function () {
+            $("#game-window").append("<div id='dino'>dino</div>")
+            $("#dino").css({ border: "2px solid white", width: "150px", height: "150px" })
+            $("#dino").css({ position: "absolute", left: "600px", bottom: "420px" })
+            this.updateHP();
+        },
+
+        updateHP: function () {
+            $("#dino").text(`${this.HP}`)
+        },
+
+        counterAttack: function () {
+            player.HP -= this.counterAttackPower;
+            player.updateHP()
+        },
+
+        takeDamage: function () {
+            console.log(player.attackPower)
+            this.HP -= player.attackPower;
+            if (this.HP <= 0) {
+                $("#dino").fadeOut()
+            } else {
+                this.updateHP();
+                this.counterAttack();
+            }
+            player.attackPower += player.baseAttackPower
+            player.updateAttackPower();
+        },
     };
 
     //********************************** KRAKEN *********************************************
     var kraken = {
-        HP: 50,
+        HP: 100,
+        counterAttackPower: 7,
         isDefending: false,
 
         defendingActivate: function () {
@@ -59,7 +119,7 @@ $(document).ready(function () {
         initialDraw: function () {
             $("#game-window").append("<div id='kraken'>Kraken</div>")
             $("#kraken").css({ border: "2px solid white", width: "150px", height: "150px" })
-            $("#kraken").css({ position: "absolute", left: "600px", bottom: "250px" })
+            $("#kraken").css({ position: "absolute", left: "600px", bottom: "235px" })
             this.updateHP();
         },
 
@@ -68,7 +128,7 @@ $(document).ready(function () {
         },
 
         counterAttack: function () {
-            player.HP -= 10;
+            player.HP -= this.counterAttackPower;
             player.updateHP()
         },
 
@@ -84,20 +144,71 @@ $(document).ready(function () {
             player.attackPower += player.baseAttackPower
             player.updateAttackPower();
         },
-    }
+    };
 
+    //********************************** CROC *********************************************
+    var croc = {
+        HP: 50,
+        counterAttackPower: 5,
+        isDefending: false,
+
+        defendingActivate: function () {
+            $('#croc').animate({ 'left': '450px' });
+            this.isDefending = true;
+        },
+
+        defendingDeactivate: function () {
+            $('#croc').animate({ 'left': '600px' });
+            this.isDefending = false;
+        },
+
+        initialDraw: function () {
+            $("#game-window").append("<div id='croc'>Croc</div>")
+            $("#croc").css({ border: "2px solid white", width: "150px", height: "150px" })
+            $("#croc").css({ position: "absolute", left: "600px", bottom: "50px" })
+            this.updateHP();
+        },
+
+        updateHP: function () {
+            $("#croc").text(`${this.HP}`)
+        },
+
+        counterAttack: function () {
+            player.HP -= this.counterAttackPower;
+            player.updateHP()
+        },
+
+        takeDamage: function () {
+            console.log(player.attackPower)
+            this.HP -= player.attackPower;
+            if (this.HP <= 0) {
+                $("#croc").fadeOut()
+            } else {
+                this.updateHP();
+                this.counterAttack();
+            }
+            player.attackPower += player.baseAttackPower
+            player.updateAttackPower();
+        },
+    };
 
     //RUN PROGRAM////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     HUD.drawPlayerAttackPower();
     HUD.drawPlayerHP();
+    HUD.drawAttackCounter();
     player.initialDraw();
     kraken.initialDraw();
+    dino.initialDraw();
+    croc.initialDraw();
 
     function processAttack(enemyObj) {
         if (enemyObj.isDefending) {
             enemyObj.takeDamage();
             enemyObj.defendingDeactivate();
+            player.attackCounter += 1;
+            player.updateAttackCounter();
+            console.log(player.attackCounter)
         } else {
             enemyObj.defendingActivate();
         }
@@ -107,6 +218,13 @@ $(document).ready(function () {
         processAttack(kraken);
     });
 
+    $("#dino").click(function () {
+        processAttack(dino);
+    });
+
+    $("#croc").click(function () {
+        processAttack(croc);
+    });
 
     //CLOSING SYNTAX////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 });
